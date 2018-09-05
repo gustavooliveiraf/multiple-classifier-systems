b85a89c93a0b4628ad77187d1e89e46792944b7f
gustavo.oliveira <gustavo.oliveira@consensotec.com.br>
2018-09-05 11:29:37 -0300

    finalizado

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from matplotlib.pylab import rcParams
 import itertools
 import seaborn as sns
 from collections import Counter
+from scipy.stats import  wilcoxon
 
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import LabelEncoder
@@ -44,7 +45,7 @@ class Main:
 
                 x_train, y_train = SMOTE().fit_sample(X_train, Y_train)
                 x_test, y_test = SMOTE().fit_sample(X_test, Y_test)
-                # print(format(Counter(y_test)))
+                # print(format(Counter(y)))   
                 
                 BaggingClassifierDecisionTree = BaggingClassifier(tree.DecisionTreeClassifier(), pool_size, max_samples, max_features, bootstrap, bootstrap_features)
                 BaggingClassifierDecisionTree.fit(x_train, y_train)
@@ -73,12 +74,23 @@ class Main:
 
         return (decisionTree, perceptron)
 
-    def plot(self, bagging, random_subspace):
+    def plot(self, bagging, random_subspace, flag):
         barWidth = 0.1
         bars = []
-        for i in range(self.metrics_length):
-            bars.append(list(itertools.chain(*bagging[0][i])))
         
+        if (flag == 0):
+            for i in range(self.metrics_length): # bagging decisionTree
+                bars.append(list(itertools.chain(*bagging[0][i])))
+        if (flag == 1):
+            for i in range(self.metrics_length): # bagging perceptron
+                bars.append(list(itertools.chain(*bagging[1][i])))
+        if (flag == 2):
+            for i in range(self.metrics_length): # random_subspace decisionTree
+                bars.append(list(itertools.chain(*random_subspace[0][i])))
+        if (flag == 3):
+            for i in range(self.metrics_length): # random_subspace perceptron
+                bars.append(list(itertools.chain(*random_subspace[1][i])))        
+        # print(bars)
         r = [np.arange((len(bars[0]))) for x in range(self.metrics_length)]
         for i in range(1, self.metrics_length):
             r[i] = [x + barWidth for x in r[i-1]]
@@ -93,10 +105,16 @@ class Main:
 
         plt.xticks([r + barWidth + 0.05 for r in range(len(bars[0])+2)], ['50%', '60%', '70%', '80%', '90%', '100%', '', ''])
         plt.xlabel('Porcentagem do conjunto de treinamento')
-        plt.ylabel('Medidas')
+        plt.ylabel('Escore')
         plt.legend()
         plt.ylim([0, 1.19])
         plt.show()
+
+    def wilcoxon_test(self, x, y):
+        p_value = wilcoxon(x, y)
+        # print(p_value)
+
+        return p_value
 
 def test(self):
     max_samples = 0.5
@@ -104,10 +122,8 @@ def test(self):
     bagging = ([[] for x in range(self.metrics_length)], [[] for x in range(self.metrics_length)])
     random_subspace = ([[] for x in range(self.metrics_length)], [[] for x in range(self.metrics_length)])
 
-
     for _ in range(self.train_length):
         print("=========== " + str(round(max_samples * 100)) + "% ===========")
-
         bagging_temp = modelo.calc_metrics(10, 1, 100, max_samples, 1.0, True, True)
         random_subspace_temp = modelo.calc_metrics(10, 1, 100, max_samples, max_features/2, True, True)
         for i in range(self.classifiers_length):
@@ -116,10 +132,14 @@ def test(self):
                 random_subspace[i][j].append(random_subspace_temp[i][j])      
         max_samples = round(max_samples + 0.1, 1)
     print("=========== plotando ===========")
-    self.plot(bagging, random_subspace)
+    self.plot(bagging, random_subspace, 0)
+    self.plot(bagging, random_subspace, 1)
+    self.plot(bagging, random_subspace, 2)
+    self.plot(bagging, random_subspace, 3)
 
 
-# TESTE
+
+# test
 # df = df.drop(axis=1, columns = ["ID"])
 # df = pd.read_csv('./entrada.csv')
 # df = pd.read_csv('./cm1.csv')
@@ -130,4 +150,6 @@ enc = LabelEncoder()
 df.CLASS = enc.fit_transform(df.CLASS)
 
 modelo = Main(df)
+# modelo.wilcoxon_test(x,y)
+
 test(modelo)
